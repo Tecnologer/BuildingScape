4b84626f027eca8b4c0ff10535d28dcd169ef067
tecnologer <contacto@tecnologer.net>
2021-04-26 22:04:29 -0500

    use materials and textures

--- Source/BuildingScape/WorldPosition.cpp
+++ Source/BuildingScape/WorldPosition.cpp
@@ -1,6 +1,7 @@
 // copyright tecnologer
 
 #include "WorldPosition.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UWorldPosition::UWorldPosition()
@@ -8,7 +9,6 @@ UWorldPosition::UWorldPosition()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
 	// ...
 }
 
@@ -16,8 +16,13 @@ UWorldPosition::UWorldPosition()
 void UWorldPosition::BeginPlay()
 {
 	Super::BeginPlay();
-
 	// ...
+	FString ActorName = GetOwner()->GetName();
+	FVector ActorPosition = GetOwner()->GetActorLocation();
+	FString ActorPositionStr = ActorPosition.ToString();
+	FString ActorTransform = GetOwner()->GetActorTransform().ToString();
+	//UE_LOG(LogTemp, Display, TEXT("Actor: %s is on X: %f, Y: %f, Z: %f"), *ActorName, ActorPosition.X, ActorPosition.Y, ActorPosition.Z);
+	UE_LOG(LogTemp, Display, TEXT("Actor: %s is on %s"), *ActorName, *ActorTransform);
 }
 
 // Called every frame
